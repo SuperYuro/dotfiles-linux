48fb044fa73cab9340e52479294ce8d3f2242ebc
SuperYuro <a.gpgtdmgp@gmail.com>
2023-06-29 16:35:30 +0900

    fix(laptop ins): remove tlp config

--- scripts/laptop/install.sh
+++ scripts/laptop/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 # Power efficiency
-sudo pacman -S tlp tlp-rdw acpi acpi_call tpacpi-bat --noconfirm
-
-sudo cp ~/dotfiles-linux/tlp/performance.conf /etc/tlp.d/
-sudo cp ~/dotfiles-linux/tlp/battery-threshold.conf /etc/tlp.d/
-
-sudo systemctl mask systemd-rfkill.service
-sudo systemctl mask systemd-rfkill.socket
-
-sudo systemctl enable --now tlp.service
-sudo tlp start
+# sudo pacman -S tlp tlp-rdw acpi acpi_call tpacpi-bat --noconfirm
+#
+# sudo cp ~/dotfiles-linux/tlp/performance.conf /etc/tlp.d/
+# sudo cp ~/dotfiles-linux/tlp/battery-threshold.conf /etc/tlp.d/
+#
+# sudo systemctl mask systemd-rfkill.service
+# sudo systemctl mask systemd-rfkill.socket
+#
+# sudo systemctl enable --now tlp.service
+# sudo tlp start
 
 # Xorg
 sudo pacman -S xorg-server xorg-apps xorg-xmodmap xorg-xinit --noconfirm
@@ -19,7 +19,7 @@ sudo pacman -S xorg-server xorg-apps xorg-xmodmap xorg-xinit --noconfirm
 sudo pacman -S i3 --noconfirm
 
 # Misc
-sudo pacman -S rofi gvfs udisks2 fwupd network-manager-applet brightnessctl --noconfirm
+sudo pacman -S rofi gvfs fwupd network-manager-applet brightnessctl lxappearance --noconfirm
 
 # Audio
 sudo pacman -S pulseaudio-alsa pavucontrol --noconfirm
