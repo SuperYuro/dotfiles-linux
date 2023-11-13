b2397a69cac74199e5bdc8cac100171ddfead4ba
SuperYuro <a.gpgtdmgp@gmail.com>
2023-11-13 19:45:43 +0900

    feat: add poetry

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -20,7 +20,7 @@ brew install tmux lsd bat dust peco ripgrep
 
 # Python
 brew install openssl readline sqlite3 xz zlib tcl-tk
-brew install pyenv rye
+brew install pyenv poetry rye
 
 # Git
 brew install ghq lazygit gh
