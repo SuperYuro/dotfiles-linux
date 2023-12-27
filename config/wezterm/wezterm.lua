4cf74a0e2d1b139bc1c4d5ed8ae35065af8e4a49
SuperYuro <a.gpgtdmgp@gmail.com>
2023-12-28 00:33:32 +0900

    feat: add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Enable IME
+config.use_ime = true
+
+-- Font
+config.font = wezterm.font("UDEV Gothic NFLG")
+config.font_size = 12.0
+
+-- Hide scrollbar
+config.enable_scroll_bar = false
+
+-- Colorscheme
+config.color_scheme = "Solarized Dark - Patched"
+
+config.window_background_opacity = 0.7
+
+-- Window titlebar
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.integrated_title_button_style = "Gnome"
+
+-- Window size
+config.initial_rows = 30
+config.initial_cols = 120
+
+config.default_prog = { "/bin/fish" }
+
+return config
