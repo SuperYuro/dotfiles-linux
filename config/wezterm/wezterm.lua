d95310c1c8ba38bd1fa2d54b560cf706b1f4684a
SuperYuro <a.gpgtdmgp@gmail.com>
2024-03-08 18:56:37 +0900

    fix(wezterm): change shell

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -17,10 +17,10 @@ config.font_size = 12.0
 config.enable_scroll_bar = false
 
 -- Colorscheme
-config.color_scheme_dir = { "~/.config/wezterm/colorscheme" }
+config.color_scheme_dirs = { "~/.config/wezterm/colorscheme" }
 config.color_scheme = "nordfox"
 
-config.window_background_opacity = 0.7
+-- config.window_background_opacity = 0.7
 
 -- Window titlebar
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
@@ -30,6 +30,7 @@ config.integrated_title_button_style = "Gnome"
 config.initial_rows = 30
 config.initial_cols = 120
 
-config.default_prog = { "/bin/fish" }
+-- config.default_prog = { "/bin/fish" }
+config.default_prog = { "/bin/zsh" }
 
 return config
